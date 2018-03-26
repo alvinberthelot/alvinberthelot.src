d0419e77fd61ac38efa146e86df3f297f536a07f
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-03-26 22:13:25 +0200

    Use webpack to generate CSS styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 console.log('Bonjour :)')
+
+import './assets/styles/index.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -41,5 +41,31 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader' // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
   }
 }
