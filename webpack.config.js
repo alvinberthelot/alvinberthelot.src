ed9cd4d6fc271a5562153522fc27dee04e49ffa8
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-03-26 21:43:58 +0200

    Init Prettier and format files

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   devServer: {
-    contentBase: "./dist"
+    contentBase: './dist'
   },
   plugins: [
-    new CleanWebpackPlugin(["dist"]),
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "src/assets/html/index.html"
+      filename: 'index.html',
+      template: 'src/assets/html/index.html'
     }),
     new HtmlWebpackPlugin({
-      filename: "events.html",
-      template: "src/assets/html/events.html"
+      filename: 'events.html',
+      template: 'src/assets/html/events.html'
     }),
     new HtmlWebpackPlugin({
-      filename: "experiences.html",
-      template: "src/assets/html/experiences.html"
+      filename: 'experiences.html',
+      template: 'src/assets/html/experiences.html'
     }),
     new HtmlWebpackPlugin({
-      filename: "langages.html",
-      template: "src/assets/html/langages.html"
+      filename: 'langages.html',
+      template: 'src/assets/html/langages.html'
     }),
     new HtmlWebpackPlugin({
-      filename: "skills.html",
-      template: "src/assets/html/skills.html"
+      filename: 'skills.html',
+      template: 'src/assets/html/skills.html'
     }),
     new HtmlWebpackPlugin({
-      filename: "stacks.html",
-      template: "src/assets/html/stacks.html"
+      filename: 'stacks.html',
+      template: 'src/assets/html/stacks.html'
     }),
     new HtmlWebpackPlugin({
-      filename: "trainings.html",
-      template: "src/assets/html/trainings.html"
+      filename: 'trainings.html',
+      template: 'src/assets/html/trainings.html'
     })
   ],
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   }
-};
+}
