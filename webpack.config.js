98272d9ba770854023bf74e8da8b1f91a516994a
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-03-26 18:01:35 +0200

    Init webpack and generate HTML files

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/assets/html/index.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "events.html",
+      template: "src/assets/html/events.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "experiences.html",
+      template: "src/assets/html/experiences.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "langages.html",
+      template: "src/assets/html/langages.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "skills.html",
+      template: "src/assets/html/skills.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "stacks.html",
+      template: "src/assets/html/stacks.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "trainings.html",
+      template: "src/assets/html/trainings.html"
+    })
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
